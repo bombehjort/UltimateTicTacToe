b250867a7a12aebf06d39f24cf082738e1f2be48
iNLoca <56917419+iNLoca@users.noreply.github.com>
2020-02-27 16:27:27 +0100

    Spacing between buttons

--- src/ultimatetictactoe/GUI/controller/MainController.java
+++ src/ultimatetictactoe/GUI/controller/MainController.java
@@ -11,6 +11,8 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -29,65 +31,64 @@ import ultimatetictactoe.move.Move;
  */
 public class MainController implements Initializable {
 
-
-   private GameManager gm;
-   IGameState gameState;
+    private GameManager gm;
+    IGameState gameState;
     @FXML
     private AnchorPane MainPane;
     @FXML
     private JFXButton closeBtn;
     @FXML
     private Pane miniPane;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         gameState = new GameState();
         gm = new GameManager(gameState);
         createAllCells();
-      }
-    private void createAllCells(){
+    }
+
+    private void createAllCells() {
         int btnWidth = 65;
         int btnHeight = 65;
-        
-        
-        for (int x = 0; x < 9; x++) {
-            for (int y = 0; y < 9; y++) {
+
+        for (int x = 0; x < 3 + 3 + 3; x++) {
+            for (int y = 0; y < 3 + 3 + 3; y++) {
+                UTTTButton btn = new UTTTButton();
+                btn.setPrefSize(btnWidth, btnHeight); 
+                btn.setMove(new Move(x, y));
                 
-        UTTTButton btn = new UTTTButton();
-        btn.setPrefSize(btnWidth, btnHeight);
-        btn.setLayoutX(6+(btnWidth+3)*x);
-        btn.setLayoutY(6+(btnHeight+3)*y);
-        btn.setMove(new Move(x,y));
-        
-        btn.setOnMouseClicked(event-> {
-            UTTTButton b =  (UTTTButton)event.getSource();
-            boolean isSucces = gm.updateGame(b.getMove());
-            String imageSource;
-            if(isSucces){
-                if(gameState.getMoveNumber()%2==0) {
-                    imageSource = "/icons/player1icon.png";
-                }
-                else{
-                    imageSource = "/icons/player2icon.png";
-                }
-                b.setGraphic(new ImageView(new Image(imageSource)));
+                int verticalSpaceBetween = 5 * (x/3);
+                int horizontalSpaceBetween = 5 * (y/3);
+                btn.setLayoutX(6 + (btnWidth + 3) * x + verticalSpaceBetween);
+                btn.setLayoutY(6 + (btnHeight + 3) * y + horizontalSpaceBetween);
+
+                btn.setOnMouseClicked(event -> {
+                    UTTTButton b = (UTTTButton) event.getSource();
+                    boolean isSucces = gm.updateGame(b.getMove());
+                    String imageSource;
+                    if (isSucces) {
+                        if (gameState.getMoveNumber() % 2 == 0) {
+                            imageSource = "/icons/player1icon.png";
+                        } else {
+                            imageSource = "/icons/player2icon.png";
+                        }
+                        b.setGraphic(new ImageView(new Image(imageSource)));
+                    }
+                });
+
+                MainPane.getChildren().add(btn);
+
             }
-        });
-                
-        MainPane.getChildren().add(btn);
-                
-            }
- 
-        }   
-       
+
+        }
+
     }
 
     @FXML
     private void clickClose(ActionEvent event) {
-         Stage stage = (Stage) closeBtn.getScene().getWindow();
+        Stage stage = (Stage) closeBtn.getScene().getWindow();
         stage.close();
-        
-    }
-    
-}    
-    
 
+    }
+
+}
